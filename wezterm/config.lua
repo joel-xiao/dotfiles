a721f3f2001483623d5070c582bf89b937037715
曦听 <44862981+joel-xiao@users.noreply.github.com>
2025-09-04 11:03:41 +0800

    Update config.lua

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -27,7 +27,9 @@ config.tab_bar_at_bottom = false
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 config.font_size = 13.5
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+-- config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+-- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "Bold" })
 config.enable_tab_bar = false
 config.window_padding = { left = 3, right = 3, top = 0, bottom = 0 }
 
